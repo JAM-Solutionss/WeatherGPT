f590c6a1d83883d118c72a15af8fa70512b3a0e4
abahde <71208362+arvedb@users.noreply.github.com>
2024-07-14 12:47:56 +0200

    added config.py for configuration of api call parameters etc.

---
+++ openmeteo/config.py
@@ -0,0 +1,21 @@
+# Configurations for API Call
+
+# Parameters for API call
+params = {
+    'latitude': 52.52,
+    'longitude': 13.41,
+    'hourly': [
+        'temperature_2m',
+        "relative_humidity_2m", 
+        "apparent_temperature", 
+        "precipitation_probability", 
+        "rain", "surface_pressure", 
+        "cloud_cover", 
+        "wind_speed_10m", 
+        "uv_index"
+    ],
+    'timezone': 'Europe/Berlin'
+}
+
+# Location - Placeholder for later
+location = {}
