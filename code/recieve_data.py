9903ea37b625e47a8949e53944ec8e5dd1ffc1bf
Miriam Renken <miriamrenken@Miriams-MBP.fritz.box>
2024-07-14 14:24:44 +0200

    fixed reciever and modules

--- code/recieve_data.py
+++ code/recieve_data.py
@@ -1,3 +1,8 @@
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from modules.llm import gpt
 from modules.weather import fetch_weater_data
 
