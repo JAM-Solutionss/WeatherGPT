580e151ba772e477c35873d6565f19d6bc9a1ee3
abahde <71208362+arvedb@users.noreply.github.com>
2024-07-14 17:45:05 +0200

    implemented "get_API_respons" function and implented proper importing in __init__.py

---
+++ modules/openmeteo/openmeteo_api.py
@@ -0,0 +1,20 @@
+import requests
+from .api_config import params
+from .data_processor import hourly_data_dict, hourly_data_dataframe
+
+def get_API_response() -> dict:
+    """Calling the openmetep API with parameters definde in api_config and returns the JSON dictionary response.
+
+    Returns:
+        dict: Returns the seponse data JSON dictionary of the API call
+    """
+    # API URL link
+    url = "https://api.open-meteo.com/v1/forecast"
+
+    # Performing API call and get response
+    response = requests.get(url, params=params)
+
+    # Getting response as dictionary
+    response_data = response.json()
+    
+    return response_data
