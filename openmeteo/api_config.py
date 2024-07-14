cbe282c68ab561c2e2743a1d74886ad97a3f3323
abahde <71208362+arvedb@users.noreply.github.com>
2024-07-14 14:22:35 +0200

    specified parameters in config

--- openmeteo/api_config.py
+++ openmeteo/api_config.py
@@ -2,23 +2,37 @@
 
 # Defining variables to be requested
 variables = [
-    'temperature_2m',
-    "relative_humidity_2m", 
-    "apparent_temperature", 
-    "precipitation_probability", 
-    "rain", "surface_pressure", 
-    "cloud_cover", 
-    "wind_speed_10m", 
-    "uv_index"
+
 ]
 
 # Parameters for API call
 params = {
     'latitude': 52.52,
     'longitude': 13.41,
-    'current': variables,
-    'hourly': variables,
-    'daily': variables,    
+    'current': [
+        'temperature_2m', 
+        'relative_humidity_2m', 
+        'apparent_temperature', 
+        'rain'
+        ],
+    'hourly': [
+        'temperature_2m',
+        'relative_humidity_2m', 
+        'apparent_temperature', 
+        'precipitation_probability', 
+        'rain', 'surface_pressure', 
+        'cloud_cover', 
+        'wind_speed_10m', 
+        'uv_index'
+    ],
+    'daily': [
+        'temperature_2m_max', 
+        'temperature_2m_min', 
+        'apparent_temperature_max', 
+        'apparent_temperature_min', 
+        'uv_index_max', 
+        'rain_sum'
+        ],    
     'timezone': 'Europe/Berlin'
 }
 
