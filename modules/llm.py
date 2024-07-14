1c2182f2f31e381d80593b2f2f6e65e228552d26
Miriam Renken <miriamrenken@Miriams-MBP.fritz.box>
2024-07-14 13:13:46 +0200

    files added

---
+++ modules/llm.py
@@ -0,0 +1,43 @@
+import os
+
+from groq import Groq
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+
+# Initialize Client
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+# Takes the data as Input and writes the weatherforcast as a txt file
+def gpt(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Du bist WeatherGPT, ein AI Assistent der auf Basis der Eingabe eine lustige Wettervorhersage macht. Antworte kurz und Knapp und immer auf Deutsch!",
+            },
+            {
+                "role": "user",
+                "content": f"{prompt} \n",
+            }
+        ],
+        model="llama3-70b-8192",
+    )
+    # prints the output of the llm for testing purpose
+    print(chat_completion.choices[0].message.content)
+    # writes the output to gpt.txt for further data processing
+    with open("gpt.txt","w") as gpt:
+        gpt.writelines(chat_completion.choices[0].message.content)
+        gpt.close()
+
+# Chat funktion for testing purpose
+def weather_gpt():
+    while True:
+        prompt = input("\n> ")
+        gpt(prompt)
+
+
+
+if __name__ == "__main__":
+    weather_gpt()
---
+++ recieve_data.py
@@ -0,0 +1,15 @@
+from modules.llm import gpt
+from modules.weather import fetch_weater_data
+
+daten = {
+    "temperature": 30,
+    "rain_percantage": 10,
+    "weather_state": "cloudy",
+    "wind": 20
+}
+
+data = fetch_weater_data()
+
+
+prompt = f"The temperature is {data['temperature_2m_max']} degrees, the rain percentage is {data['rain_sum']}, the wind speed is {data['wind_speed_10m_max']}"
+gpt(prompt)
