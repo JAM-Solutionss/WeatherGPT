1a474266dd4d879e4686955711dff97c0aed0e94
Miriam Renken <miriamrenken@Miriams-MBP.fritz.box>
2024-07-15 13:01:17 +0200

    fixed the errors I got. Now only the Attribute problem still exists

--- all_imports.py
+++ all_imports.py
@@ -2,7 +2,6 @@
 import sys
 import os
 
-<<<<<<< HEAD
 #from codebase import *
 
 from modules.llm import gpt
@@ -14,16 +13,5 @@ from codebase.recieve_data import get_llm_response
 
 
 
-=======
-# from code.gui import Gui
-# from code.recieve_data import get_llm_response
-from modules.openmeteo import get_API_response, hourly_data_dict
->>>>>>> 5110abf9ae847afa19a6ce13cd6082fd3b3c214d
 
 
-<<<<<<< HEAD
-=======
-from modules.speech import create_audio
-
-hallo = 1234
->>>>>>> 5110abf9ae847afa19a6ce13cd6082fd3b3c214d
