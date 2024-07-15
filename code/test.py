37352e5b540d36884f89ccedf98a2837eb1e7860
Miriam Renken <miriamrenken@Miriams-MBP.fritz.box>
2024-07-15 12:53:12 +0200

    folder deleted

--- code/test.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-
-# Fügen Sie den relativen Pfad zu dem Verzeichnis 'code' und 'all_imports' hinzu
-project_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(project_path)
-
-from all_imports import hallo
-
-print(hallo)
